a18fb1a6f43c0a5fd7bc09606332ad51228a96ea
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2015-07-30 03:37:19 -0300

    Lock down specific projects controller actions for admins only

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+
   def title(*parts)
     unless parts.empty?
       content_for :title do
@@ -6,4 +7,15 @@ module ApplicationHelper
       end
     end
   end
+
+
+#This try method tries a
+#method on an object, and if that method doesn't exist (as it wouldn't if
+#current_user were nil ), then it gives up and returns nil , rather than raising
+#a NoMethodError exception.
+
+  def admins_only(&block)
+    block.call if current_user.try(:admin?)
+  end
+
 end
